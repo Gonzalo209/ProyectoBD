fbdb708f44c5588f3da4f96ac781494b1116825e
Gonzalo209 <cgonzalo983@gmail.com>
2024-01-10 10:19:04 -0600

    jajjax3

--- src/main/java/org/example/control/ValidacionDecimalTallaControl.java
+++ src/main/java/org/example/control/ValidacionDecimalTallaControl.java
@@ -1,21 +1,27 @@
 package org.example.control;
 
 import org.example.vista.MensajeVista;
+import org.example.vista.VentanaPrincipalVista;
 
-import javax.swing.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
-public class ValidacionNumerosControl extends KeyAdapter {
+public class ValidacionDecimalTallaControl extends KeyAdapter {
 
     @Override
     public void keyTyped(KeyEvent e) {
 
         boolean numero = e.getKeyChar() >= '0' && e.getKeyChar() <= '9' || (int) e.getKeyChar() == 8;
 
-        if(! numero) {
+        if(! numero && e.getKeyChar() != '.') {
 
             MensajeVista.mensajeError("Ingrese solo numeros", "Error");
+
+            e.consume();
+
+        }
+
+        if(e.getKeyChar() == '.' && VentanaPrincipalVista.getTxtTalla().getText().contains(".")) {
 
             e.consume();
 
@@ -24,3 +30,4 @@ public class ValidacionNumerosControl extends KeyAdapter {
     }
 
 }
+
